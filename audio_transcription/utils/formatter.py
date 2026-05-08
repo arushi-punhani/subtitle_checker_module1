7f0e5d4c7d2ef7a60fc17dffbe934f8c4235c33c
Arushi Punhani <arushi.punhani@gmail.com>
2026-05-08 12:04:35 +0530

    added formatter.py

---
+++ audio_transcription/utils/formatter.py
@@ -0,0 +1,6 @@
+def format_timestamp(seconds):
+    hrs = int(seconds // 3600)
+    mins = int((seconds % 3600) // 60)
+    secs = int(seconds % 60)
+    millis = int((seconds - int(seconds)) * 1000)
+    return f"{hrs:02}:{mins:02}:{secs:02},{millis:03}"
